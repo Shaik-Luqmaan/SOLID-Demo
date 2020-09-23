6c366d178cd48c959069a6d7fb03f93e7215659a
Shaik Luqmaan <luqmaan.shaik@zemosolabs.com>
2020-09-23 12:12:09 +0530

    Refractored Code

--- SolidDemo.java
+++ SolidDemo.java
@@ -1,87 +1,89 @@
 package Solid_Demo;
 
-//Interface Segregation
-interface Order{
+interface IOrder {
     public void orderDetails();
+
 }
-interface Payment{
+interface IPayment {
     public void paymentDetails();
 }
 
-interface billInfo{
+interface IBillInformation {
     void billDetails(String info);
 }
 
-class orderInfo implements Order{
+class OrderInformation implements IOrder {
     public String order;
-    orderInfo(String order){
+    OrderInformation(String order){
         this.order=order;
     }
     @Override
     public void orderDetails() {
-        System.out.println("The dish ordered is: "+order);
+        System.out.println(
+                "The dish ordered is: "+order
+        );
     }
 }
-
-//Open-Closed, LSP
-class Restaurant extends orderInfo implements Order{
+class Restaurant extends OrderInformation implements IOrder {
     public int time;
     Restaurant(String order,int time){
         super(order);
         this.time=time;
     }
-    public void prepare(){
-        System.out.println("Your order "+order+" will be ready in "+time+" mins");
+    public void prepareOrder(){
+        System.out.println(
+                "Your order "+order+" will be ready in "+time+" mins"
+        );
     }
     @Override
     public void orderDetails() {
         super.orderDetails();
-        System.out.println("Your order has been accepted by the Restaurant");
+        System.out.println(
+                "Your order has been accepted by the Restaurant"
+        );
     }
 }
-//Single Responsibility
-class paymentInfo implements Payment{
-    protected String paymode;
+class PaymentInformation implements IPayment {
+    protected String paymentMode;
     private double amount;
-    paymentInfo(String paymode,double amount){
+    PaymentInformation(String paymentMode, double amount){
         this.amount=amount;
-        this.paymode=paymode;
+        this.paymentMode = paymentMode;
 
     }
     @Override
     public void paymentDetails() {
-        System.out.println("Payment done through "+paymode+" and the amount is: "+amount);
+        System.out.println(
+                "Payment done through "+ paymentMode +" and the amount is: "+amount
+        );
     }
 }
-
-//Dependency Inverse
-
-class checkBill implements billInfo {
+class CheckBill implements IBillInformation {
     public void billDetails(String info){
-        System.out.println("Bill Status: "+info);
+        System.out.println(
+                "Bill Status: "+info
+        );
     }
 }
-
-class billStatus{
-    private billInfo b;
-    billStatus(billInfo b){
-        this.b=b;
+class BillStatus{
+    private IBillInformation IBillInformation;
+    BillStatus(IBillInformation IBillInformation){
+        this.IBillInformation = IBillInformation;
     }
     public void status(){
-        b.billDetails("The bill has been paid");
+        IBillInformation.billDetails("The bill has been paid");
     }
-
 }
 
 public class SolidDemo {
     public static void main(String[] args) {
-        Restaurant r=new Restaurant("Dosa",10);
-        r.orderDetails();
-        r.prepare();
-        billStatus obj=new billStatus(new checkBill());
-        obj.status();
-        paymentInfo p=new paymentInfo("Netbanking",30);
-        p.paymentDetails();
+        Restaurant restaurantObject=new Restaurant("Dosa",10);
+        restaurantObject.orderDetails();
+        restaurantObject.prepareOrder();
+        BillStatus billObject=new BillStatus(new CheckBill());
+        billObject.status();
+        PaymentInformation paymentObject=new PaymentInformation("Netbanking",30);
+        paymentObject.paymentDetails();
       ;
     }
 }
